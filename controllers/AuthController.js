85f9d857204e673c8d56c05435118684cd0d6a44
Zeyad Elnaggar <zeyadosama15@gmail.com>
2025-01-31 04:01:10 +0200

    Authentication

---
+++ controllers/AuthController.js
@@ -0,0 +1,40 @@
+import sha1 from 'sha1';
+import v4 from 'uuid';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AuthController {
+  static async getConnect(req, res) {
+    const header = req.headers.authorization;
+    const authorization = header.split(' ')[1];
+    if (!authorization) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const decodedString = Buffer.from(authorization, 'base64').toString('utf-8');
+    const [email, password] = decodedString.split(':');
+
+    const user = await dbClient.db.collection('users').findOne({ email });
+    if (!user || user.password !== sha1(password)) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const token = v4();
+    const hours = 24;
+    await redisClient.setex(`auth_${token}`, user._id.toString(), hours * 3600);
+
+    return res.status(200).send({ token });
+  }
+
+  static async getDisconnect(req, res) {
+    const token = req.headers['x-token'];
+
+    const result = await redisClient.del(`auth_${token}`);
+    if (result === 0) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    return res.status(204).send();
+  }
+}
+
+export default AuthController;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 function routing(app) {
   const router = express.Router();
@@ -15,6 +16,18 @@ function routing(app) {
 
   router.post('/users', (req, res) => {
     UsersController.postNew(req, res);
+  });
+
+  router.get('/connect', (req, res) => {
+    AuthController.getConnect(req, res);
+  });
+
+  router.get('/disconnect', (req, res) => {
+    AuthController.getDisconnect(req, res);
+  });
+
+  router.get('/users/me', (req, res) => {
+    UsersController.getMe(req, res);
   });
 }
 
