d6d79a322949db1cbfd2c1551a7ccf5125f229c5
Zeyad Elnaggar <zeyadosama15@gmail.com>
2025-01-31 02:04:09 +0200

    using injectRoutes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
+// eslint-disable-next-line no-unused-vars
 import express from 'express';
 import { getStatus, getStats } from '../controllers/AppController';
 
-const router = express.Router();
-router.get('/status', getStatus);
-router.get('/stats', getStats);
+// const router = express.Router();
+// router.get('/status', getStatus);
+// router.get('/stats', getStats);
 
-export default router;
+// export default router;
+
+/**
+ * Injects routes with their handlers to the given Express application.
+ * @param {Express} api
+ */
+const injectRoutes = (api) => {
+  api.get('/status', getStatus);
+  api.get('/stats', getStats);
+};
+
+export default injectRoutes;
